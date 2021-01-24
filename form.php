8f7f828c46e45cd2457e35d69cfe96cce760c360
sewanna <seweryniakanna1@gmail.com>
2021-01-24 17:03:43 +0100

    24.01.2020

---
+++ form.php
@@ -0,0 +1,8 @@
+<?php
+mail(
+    "adresse@.de", 
+    "Es gibt eine neue Kontaktanfrage", 
+    "Email: {$_POST['email']}, Name: {$_POST['name']}, Nachricht: {$_POST['message']}");
+
+header("Location: index.php?contacted=1");
+?>
