eab9b38f7d8ec9dfcc873bc3bb7fefa985d9f2d4
lena wrotz <seweryniakanna1@gmail.com>
2021-11-21 17:11:14 +0100

    21.11.2021

--- bootcamp-jannis.php
+++ bootcamp-jannis.php
@@ -1,4 +1,4 @@
-
+<!----Zadanie 1 - oblicz index BMI ------->
 <?php
 //echo 6 + 5 ; 
 //echo "6+5" ; //jak w js-cudzysłów to string
@@ -8,12 +8,28 @@
 //echo $number1 / 2 + $number1;
 //echo $number1 ++; //powiekszenie o 1 -skrócona 
 //echo $number1 --; //pomniejszenie o 1 -skrócona 
-echo "BMI wynosi:" ;
+echo "BMI wynosi: <br/>" ;
 $wzrost = 1.70;
 $waga = 65 ;
 $BMI = $waga / ( $wzrost * $wzrost ) ;
-echo $BMI
+echo $BMI ;
 //echo ($wzrost / ($waga * $waga)) ; inna forma zapisu
+//echo "BMI wynosi: {$BMI}"; inna forma zapisu.Zmienną jak w js moze byc i string i number
+echo strlen ($waga); //funkcja string length- wynik pokazuje liczbe znaków w zmiennej,lącznie ze spacjami
+var_dump ($waga); //konwersja- funkcja wskazuje wartość i mowi jak interpretuje php zmienna. Init to  number, 
+//float- number z wart. po przecinku, string to string , boolen- jesli zmienna jest true/false, poda to; 
+//Do boolen jest jeszcze opcja (true && false) -true AND false,
+// (true||false) - true OR false 
+$funkcja = "" ;
+var_dump(empty($funkcja)); //funkcja empty  bedzie miala zawsze wartosc true jeści zmienna jest pusta/liczba zero/string zero
+//w kazdym innym przypadku funkcja empty bedzie miała wartoś false
+$funkcja11 = "" ;
+var_dump(isset($funkcja1)); //funkcja isset zawsze bedzie miala wartosc true, false bedzie tylko w przypadku gdy zmienna nie bedzie okreslona
+//to dobra funkcja do sprawdzania czy zmienna jest  okreslona
+
+$funkcja2 ="";
+unset ($funkcja2); //unset- funkcja usuwa zmienna
+var_dump(isset($funkcja2)); //pokazuje w wyniku false, bo fukcja2 nie istnieje- zostala wczesniej usunieta przez unset
 
 ?>
 
